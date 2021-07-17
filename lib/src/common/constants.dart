a35a5f34675dd60eaa1410ba5003c74050340ad0
Francisco Barrera Gil <francisco.barrera.gil@gmail.com>
2021-07-17 17:58:26 -0400

    Revert "Fixed notification url and redirect url"
    This reverts commit 2ed49382d93ab41308e31d2b96a3df9f3ecc0f2a.

--- lib/src/common/constants.dart
+++ lib/src/common/constants.dart
@@ -1,5 +1,5 @@
 final String fITBITURL =
-    "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=23B5JZ&redirect_uri=https%3A%2F%2Ffrancisforte.cl%2Fcallback&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
+    "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=23B5JZ&redirect_uri=https%3A%2F%2Ffrancisforte.cl&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
 
 String getFitbitUrl() {
   return fITBITURL;
